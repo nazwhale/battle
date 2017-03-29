9f67f3e135cece612ff257a64e34a376e5361718
Naz <nazmalik08@gmail.com>
2017-03-29 16:44:07 +0100

    add feature and test to display player HP

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,11 +1,19 @@
 require_relative '../../app'
 
-  feature 'checks / displays testing infrastructure message' do
+  feature 'testing infrastructure' do
+    before do
+      visit '/'
+      fill_in :player_1, with: 'Alex'
+      fill_in :player_2, with: 'Naz'
+      click_button 'Submit Player Names'
+    end
+
     scenario 'players can input names in a form' do
-      visit "/"
-        fill_in :player_1, with: 'Alex'
-        fill_in :player_2, with: 'Naz'
-        click_button 'Submit Player Names'
       expect(page).to have_content("Alex the Hippy with Naz the Hippy")
     end
-end
+    
+    scenario 'displays player 2 HP' do
+      expect(page).to have_content("Naz's HP: 1")
+    end
+
+  end

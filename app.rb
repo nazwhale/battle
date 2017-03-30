058a1c6ab56b27be018251107c539cd587d07b9d
Naz <nazmalik08@gmail.com>
2017-03-30 12:39:55 +0100

    change session to $ variables

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require './lib/player'
 
 class Battle < Sinatra::Base
 
@@ -9,14 +10,16 @@ enable :sessions
   end
 
   post '/player_names' do
-    session[:player_1] = params[:player_1]
-    session[:player_2] = params[:player_2]
+    $player_1 = Player.new(params[:player_1])
+    $player_2 = Player.new(params[:player_2])
+#    session[:player_1] = params[:player_1]
+#    session[:player_2] = params[:player_2]
     redirect '/play'
   end
 
   get '/play' do
-    @player_1 = session[:player_1]
-    @player_2 = session[:player_2]
+    @player_1 = $player_1 
+    @player_2 = $player_2 
     erb :play
   end
 
